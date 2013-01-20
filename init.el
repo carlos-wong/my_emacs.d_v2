f0f2b21a0f2a26916b186aa4437c6be608980b5c
Carlos <huaixian.huang@gmail.com>
2013-01-20 10:45:47 +0800

    base setting from old added on starter-kit

---
+++ init.el
@@ -0,0 +1,95 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+;; Add in your own as you wish:
+(defvar my-packages '(starter-kit)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
+(auto-image-file-mode)
+;;;让 Emacs 可以直接打开和显示图片
+; 
+(fset 'yes-or-no-p 'y-or-n-p) ; 将yes/no替换为y/n
+(global-linum-mode t)
+(cua-mode 1)
+(setq frame-title-format "%f") ; 显示当前编辑的文档
+
+(auto-image-file-mode)
+;;;让 Emacs 可以直接打开和显示图片
+
+(global-set-key (kbd "C-x t") 'org-clock-in)
+(global-set-key (kbd "C-x s") 'org-clock-out)
+
+(setq compilation-scroll-output t)
+
+(global-set-key [C-tab] 'other-window);;切换到另一个窗口，快捷键为
+;;C+Tab
+
+
+(global-set-key "%" 'match-paren)
+(show-paren-mode t)          
+(defun match-paren (arg)
+  "Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+	((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+	(t (self-insert-command (or arg 1)))))
+
+;;以空行结束
+(setq require-final-newline t)
+
+
+
+(set-default-font "-adobe-Source Code Pro-normal-normal-normal-*-13-*-*-*-m-0-iso10646-1")
+
+
+;;代码折叠
+(add-hook 'c-mode-hook 'hs-minor-mode)
+(add-hook 'c++-mode-hook 'hs-minor-mode)  
+(add-hook 'python-mode-hook 'hs-minor-mode)
+
+
+(dolist (command '(yank yank-pop))
+  (eval
+   `(defadvice ,command (after indent-region activate)
+      (and (not current-prefix-arg)
+           (member major-mode
+                   '(emacs-lisp-mode
+                     lisp-mode
+                     clojure-mode
+                     scheme-mode
+                     haskell-mode
+                     ruby-mode
+                     rspec-mode
+                     python-mode
+                     c-mode
+                     c++-mode
+                     objc-mode
+                     latex-mode
+                     js-mode
+                     plain-tex-mode))
+           (let ((mark-even-if-inactive transient-mark-mode))
+             (indent-region (region-beginning) (region-end) nil))))))
+
+(defun qiang-comment-dwim-line (&optional arg)
+  "Replacement for the comment-dwim command.
+If no region is selected and current line is not blank and we are not at the end of the line,
+then comment current line.
+Replaces default behaviour of comment-dwim, when it inserts comment at the end of the line."
+  (interactive "*P")
+  (comment-normalize-vars)
+  (if (and (not (region-active-p)) (not (looking-at "[ \t]*$")))
+      (comment-or-uncomment-region (line-beginning-position) (line-end-position))
+    (comment-dwim arg)))
+(global-set-key "\M-;" 'qiang-comment-dwim-line)
+
+(defun insert-current_time ()
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d %H:%M:%S" (current-time))))
+
