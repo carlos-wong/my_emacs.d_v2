c3aa5d74e437128e22c3bd7a884f63b659d3fa19
Carlos <huaixian.huang@gmail.com>
2013-01-20 22:21:54 +0800

    add gitignore add use C-x m to trigger smex

--- init.el
+++ init.el
@@ -151,3 +151,5 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(global-set-key (kbd "C-x m") 'smex)
