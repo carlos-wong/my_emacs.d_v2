95d86315658c4c9caa9c4af61d34d90641909ddf
carlos <huaixian.huang@gmail.com>
2013-01-21 21:24:41 +0800

    chang some argv

--- init.el
+++ init.el
@@ -12,6 +12,11 @@
 (defvar my-packages '(starter-kit auto-complete org)
   "A list of packages to ensure are installed at launch.")
 
+(setq ac-ignore-case 'smart)
+;; Show 0.8 second later
+(setq ac-auto-show-menu 0.8)
+(setq ac-auto-start 4)
+
 (dolist (p my-packages)
   (when (not (package-installed-p p))
     (package-install p)))
@@ -30,7 +35,7 @@
 (auto-image-file-mode)
 ;;;让 Emacs 可以直接打开和显示图片
 
-(setq default-fill-column 80);默认显示 80列就换行
+(setq default-fill-column 70);默认显示 80列就换行
 (global-linum-mode t)
 
 (if (eq system-type 'gnu/linux)
@@ -126,6 +131,7 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
     (comment-dwim arg)))
 (global-set-key "\M-;" 'qiang-comment-dwim-line)
 
+
 (defun insert-current_time ()
   (interactive)
   (insert (format-time-string "%Y-%m-%d %H:%M:%S" (current-time))))
@@ -180,12 +186,51 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
 ;; (global-set-key (kbd "C-x C-s") 'magit-status)
 (require 'org)
 
-(global-set-key "\C-x\C-m" 'smex)
-(global-set-key "\C-c\C-m" 'smex)
+(global-set-key "\C-x\m" 'smex)
+(global-set-key "\C-c\m" 'smex)
 
 (global-set-key "\C-w" 'backward-kill-word)
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-c\C-k" 'kill-region)
+
+
+;; (if (eq system-type 'gnu/linux)
+;;     (setq org-default-notes-file "/tmp/.notes")
+;;   )
+
+;; author: pluskid
+;; 调用 stardict 的命令行程序 sdcv 来查辞典
+;; 如果选中了 region 就查询 region 的内容，否则查询当前光标所在的单词
+;; 查询结果在一个叫做 *sdcv* 的 buffer 里面显示出来，在这个 buffer 里面
+;; 按 q 可以把这个 buffer 放到 buffer 列表末尾，按 d 可以查询单词
+(global-set-key (kbd "C-c d") 'kid-sdcv-to-buffer)
+(defun kid-sdcv-to-buffer ()
+  (interactive)
+  (let ((word (if mark-active
+                  (buffer-substring-no-properties (region-beginning) (region-end))
+                (current-word nil t))))
+    (setq word (read-string (format "Search the dictionary for (default %s): " word)
+                            nil nil word))
+    (set-buffer (get-buffer-create "*sdcv*"))
+    (buffer-disable-undo)
+    (erase-buffer)
+    (let ((process (start-process-shell-command "sdcv" "*sdcv*" "sdcv" "-n" word)))
+      (set-process-sentinel
+       process
+       (lambda (process signal)
+         (when (memq (process-status process) '(exit signal))
+           (unless (string= (buffer-name) "*sdcv*")
+             (setq kid-sdcv-window-configuration (current-window-configuration))
+             (switch-to-buffer-other-window "*sdcv*")
+             (local-set-key (kbd "d") 'kid-sdcv-to-buffer)
+             (local-set-key (kbd "q") (lambda ()
+                                        (interactive)
+                                        (bury-buffer)
+                                        (unless (null (cdr (window-list))) ; only one window
+                                          (delete-window)))))
+           (goto-char (point-min))))))))
+
+
 
 
 (custom-set-variables
@@ -193,7 +238,8 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (misterioso))))
+ '(custom-enabled-themes (quote (misterioso)))
+)
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
