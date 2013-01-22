22605ff36bee8ed5a60d99fe774c0ea1796587a8
carlos <huaixian.huang@gmail.com>
2013-01-22 21:02:56 +0800

    add color theme zenburn for magit better look

--- init.el
+++ init.el
@@ -265,13 +265,13 @@ Replaces default behaviour of comment-dwim, when it inserts comment at the end o
 
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(custom-enabled-themes (quote (misterioso)))
+ '(custom-enabled-themes (quote (zenburn)))
  '(custom-safe-themes (quote ("4c8f0d2ccaced4349d7ef6d5c17f77cf97655a6f247bf1edf00699b235dea964" default)))
  '(org-startup-truncated nil))
 
